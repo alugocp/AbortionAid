0db1f654a2e8ed66a645ddb40cb4552f6c60309f
Alex Lugo <alugocp@aim.com>
2017-02-22 21:30:32 -0500

    Add files via upload

--- scripts/startup.js
+++ scripts/startup.js
@@ -2,11 +2,16 @@
 
 $.get("content.xml",function(xml){
   engine.setupContent($(xml).children(0));
-  $("#headermargin").height($("#header").innerHeight()+$("#sections").innerHeight());
   engine.goToSection("welcome");
   var children=$("#sections").children();
   children.first().css("border-bottom-left-radius","10px");
   children.first().css("border-left-width","0px");
   children.eq(children.length-1).css("border-bottom-right-radius","10px");
-  $("#des").css("top",$("#header").innerHeight()+$("#sections").innerHeight());
+  setTimeout(function(){
+    $("body").css("background-color","white");
+    $("load").css("display","none");
+    $("#main").css("display","block");
+    $("#headermargin").height($("#header").innerHeight()+$("#sections").innerHeight());
+    $("#des").css("top",$("#header").innerHeight()+$("#sections").innerHeight());
+  },500);
 });

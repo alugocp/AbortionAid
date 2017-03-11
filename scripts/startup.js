1944bd1ac7079219d1f8d89f325e139e5c9ca028
Alex Lugo <alugocp@aim.com>
2017-03-11 09:15:37 -0500

    Add files via upload

--- scripts/startup.js
+++ scripts/startup.js
@@ -9,7 +9,20 @@ $.get("content.xml",function(xml){
   children.eq(children.length-1).css("border-bottom-right-radius","10px");
   $("#headermargin").height($("#header").innerHeight()+$("#sections").innerHeight());
   $("#des").css("top",$("#header").innerHeight()+$("#sections").innerHeight());
+  $("#header").height($("#header").height()+$("#sections").outerHeight());
+
+  engine.min=40;
+  $("#sections").children().each(function(){
+    engine.min+=$(this).outerWidth();
+  });
   setTimeout(function(){
     Lodr.display();
   },500);
+});
+$(window).resize(function(){
+  if($(window).width()<engine.min){
+    $("#header").css("overflow-x","scroll");
+  }else{
+    $("#header").css("overflow-x","auto");
+  }
 });
